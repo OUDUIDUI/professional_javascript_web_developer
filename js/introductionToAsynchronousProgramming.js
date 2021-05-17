04792f33596f87498bae1542ad198b507c794ace
oushihao <oushihao97@163.com>
2021-05-17 15:08:37 +0800

    introduction To Asynchronous Programming

--- js/introductionToAsynchronousProgramming.js
+++ js/introductionToAsynchronousProgramming.js
@@ -1,3 +1,12 @@
-(function () {
-    console.log("------------ 箭头函数 ------------");
+(async function () {
+    console.log("------------ 异步编程 ------------");
+    console.log('同步行为1');
+    console.log('同步行为2');
+    setTimeout(() => console.log('异步行为'));
+    console.log('同步行为3');
+
+    function double(value, callback) {
+        setTimeout(() => callback(value * 2), 1000);
+    }
+    double(3 , (x) => console.log(`获取到异步返回值：x = ${x}`));
 })()
